216b910ac8e7ef773c79aeb54c45715f00ab21e4
Rachel <rkp32@nau.edu>
2025-07-08 12:47:14 -0700

    Relocated all logos and icons and converted their references to components

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -1,16 +1,21 @@
 import { NavLink } from 'react-router'
+import CartIcon from '../assets/images/icons/cart-icon.png';
+import SearchIcon from '../assets/images/icons/search-icon.png';
+import LogoWhite from '../assets/images/logo-white.png';
+import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
 import './Header.css'
 
 export function Header() {
   return (
     <>
+
       <div className="header">
         <div className="left-section">
           <NavLink to="/" className="header-link">
             <img className="logo"
-              src="images/logo-white.png" />
+              src={LogoWhite} />
             <img className="mobile-logo"
-              src="images/mobile-logo-white.png" />
+              src={MobileLogoWhite} />
           </NavLink>
         </div>
 
@@ -18,7 +23,7 @@ export function Header() {
           <input className="search-bar" type="text" placeholder="Search" />
 
           <button className="search-button">
-            <img className="search-icon" src="images/icons/search-icon.png" />
+            <img className="search-icon" src={SearchIcon} />
           </button>
         </div>
 
@@ -29,7 +34,7 @@ export function Header() {
           </NavLink>
 
           <NavLink className="cart-link header-link" to="/checkout">
-            <img className="cart-icon" src="images/icons/cart-icon.png" />
+            <img className="cart-icon" src={CartIcon} />
             <div className="cart-quantity">3</div>
             <div className="cart-text">Cart</div>
           </NavLink>
