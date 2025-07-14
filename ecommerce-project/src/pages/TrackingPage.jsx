f524cb207a3d3dbd473b850c331cf6703244db41
Rachel <rkp32@nau.edu>
2025-07-14 15:04:55 -0700

    Passed cart as a prop to TrackingPage header. Changed the URL to link to have the orderID and productID in the URL

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -3,12 +3,12 @@ import { Header } from '../components/Header'
 import '../components/Header.css'
 import './TrackingPage.css'
 
-export function TrackingPage() {
+export function TrackingPage({ cart }) {
   return (
     <>
       <title>Tracking</title>
       <link rel="icon" type="image/svg+xml" href="tracking-favicon.png" />
-      <Header />
+      <Header cart={cart} />
 
       <div className="tracking-page">
         <div className="order-tracking">
