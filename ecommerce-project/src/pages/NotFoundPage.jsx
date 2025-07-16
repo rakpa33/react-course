0eca0916b5e04c2a85d25912d387f13ed51b665c
Rachel <rkp32@nau.edu>
2025-07-16 15:20:41 -0700

    Passed the cart into the 404 page

--- ecommerce-project/src/pages/NotFoundPage.jsx
+++ ecommerce-project/src/pages/NotFoundPage.jsx
@@ -1,10 +1,10 @@
 import { Header } from '../components/Header'
 import './NotFoundPage.css'
 
-export function NotFoundPage() {
+export function NotFoundPage({ cart }) {
   return (
     <>
-      <Header />
+      <Header cart={cart} />
       <div className="not-found-content">
         <h1>Page not found</h1>
       </div>
