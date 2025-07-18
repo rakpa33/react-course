9b445cc690c198a58cba057cf58ca932ba288999
Rachel <rkp32@nau.edu>
2025-07-18 15:34:39 -0700

    Quantity link now toggles input field to update item quantity in cart

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -1,7 +1,26 @@
 import axios from 'axios';
 import { formatMoney } from '../../utils/money'
+import { useState } from 'react';
 
 export function CartItemDetails({ cartItem, loadCart }) {
+  const [showQuantityInput, setShowQuantityInput] = useState(false);
+  const [quantity, setQuantity] = useState(cartItem.quantity);
+
+  const toggleShowQuantityInput = async (quantity) => {
+    if (showQuantityInput) {
+      await axios.put(`api/cart-items/${cartItem.productId}`, { quantity: Number(quantity) });
+      await loadCart();
+      setShowQuantityInput(false);
+    } else {
+      setShowQuantityInput(true);
+    }
+  };
+
+  const updateQuantityInput = (event) => {
+    setQuantity(event.target.value);
+    console.log(quantity);
+  };
+
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
     await loadCart();
@@ -21,16 +40,17 @@ export function CartItemDetails({ cartItem, loadCart }) {
         </div>
         <div className="product-quantity">
           <span>
-            Quantity: <span className="quantity-label">{cartItem.quantity}</span>
+            Quantity: {showQuantityInput ? <input className="update-quantity-input" type="text" defaultValue={quantity} onChange={updateQuantityInput} /> : <span className="quantity-label" value={quantity}>{cartItem.quantity}</span>
+            }
           </span>
-          <span className="update-quantity-link link-primary">
+          <span className="update-quantity-link link-primary" onClick={() => { toggleShowQuantityInput(quantity) }}>
             Update
           </span>
           <span className="delete-quantity-link link-primary" onClick={deleteCartItem}>
             Delete
           </span>
         </div>
-      </div>
+      </div >
     </>
   );
 }
