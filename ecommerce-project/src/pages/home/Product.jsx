36b500a3ae3718fa4aec8ad84ea33c48885994b0
Rachel <rkp32@nau.edu>
2025-07-18 13:26:09 -0700

    Show "Added" message for 2 sec when clicking "Add to Cart"

--- ecommerce-project/src/pages/home/Product.jsx
+++ ecommerce-project/src/pages/home/Product.jsx
@@ -5,6 +5,7 @@ import CheckmarkIcon from '../../assets/images/icons/checkmark.png'
 
 export function Product({ product, loadCart }) {
   const [quantity, setQuantity] = useState(1);
+  const [showAddedMessage, setShowAddedMessage] = useState(false);
 
   const addToCart = async () => {
     await axios.post('/api/cart-items', {
@@ -12,7 +13,14 @@ export function Product({ product, loadCart }) {
       quantity
     });
     await loadCart();
+    setShowAddedMessage(true);
+
+    setTimeout(() => {
+      setShowAddedMessage(false);
+    }, 2000);
   };
+
+
 
   const selectQuantity = (event) => {
     const quantitySelected = Number(event.target.value);
@@ -59,7 +67,7 @@ export function Product({ product, loadCart }) {
 
       <div className="product-spacer"></div>
 
-      <div className="added-to-cart">
+      <div className="added-to-cart" style={{ opacity: showAddedMessage ? 1 : 0 }}>
         <img src={CheckmarkIcon} />
         Added
       </div>
