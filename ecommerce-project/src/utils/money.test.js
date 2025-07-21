872f8cfb72ed8dc6a15ebd99150f56000e10f29b
Rachel <rkp32@nau.edu>
2025-07-21 15:12:08 -0700

    formatMoney tests to check for $0 and negative dollar amounts

--- ecommerce-project/src/utils/money.test.js
+++ ecommerce-project/src/utils/money.test.js
@@ -11,4 +11,16 @@ describe('formatMoney', () => {
     expect(formatMoney(1090)).toBe('$10.90');
     expect(formatMoney(100)).toBe('$1.00');
   };
+
+  it('formats the number 0 as $0.00'), () => {
+    expect(formatMoney(0)).toBe('$0.00');
+  };
+
+  it('formats -999 as -$9.99'), () => {
+    expect(formatMoney(-999)).toBe('$9.99');
+  }
+
+  it('formats -100 as -$1.00'), () => {
+    expect(formatMoney(-100)).toBe('$1.00');
+  }
 });
