c2f080786186c0e6b1ed31b5852775494a3d35b3
Rachel <rkp32@nau.edu>
2025-07-22 10:43:00 -0700

    Update the title in Chatbot project and installed React Compiler

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -5,14 +5,15 @@ import ChatMessages from './components/ChatMessages'
 import './App.css'
 
 function App() {
-  const [chatMessages, setChatMessages] = useState(JSON.parse(localStorage.getItem('chatMessages')) || [],[]);
-  
+  const [chatMessages, setChatMessages] = useState(JSON.parse(localStorage.getItem('chatMessages')) || [], []);
+  let title = `${chatMessages.length} Messages`;
+
   useEffect(() => {
     Chatbot.addResponses({
       'goodbye': 'Goodbye! Have a great day!',
       'give me a unique id': () => {
         return `Sure! Here's a unique ID: ${crypto.randomUUID()}`;
-        }
+      }
     })
   }, []);
 
@@ -21,19 +22,24 @@ function App() {
   }, [chatMessages]);
 
   return (
-    <div className='app-container'>
-      {chatMessages.length === 0 && (
-        <p className='welcome-message'>
-          Welcome to the chatbot project! Send a message using the textbox
-          below.
-        </p>
-      )}
-      <ChatMessages chatMessages={chatMessages} />
-      <ChatInput
-        chatMessages={chatMessages}
-        setChatMessages={setChatMessages}
-      />
-    </div>
+    <>
+      <title>{title}</title>
+      <link rel="icon" type="image/svg+xml" href="src/assets/robot.png" />
+
+      <div className='app-container'>
+        {chatMessages.length === 0 && (
+          <p className='welcome-message'>
+            Welcome to the chatbot project! Send a message using the textbox
+            below.
+          </p>
+        )}
+        <ChatMessages chatMessages={chatMessages} />
+        <ChatInput
+          chatMessages={chatMessages}
+          setChatMessages={setChatMessages}
+        />
+      </div>
+    </>
   );
 }
 
